805c2407767765ee15c3850eb1b9a50b1f5eac5f
fykoulas <spyrosfykas@gmail.com>
2024-10-26 18:20:20 +0300

    26/10/2024

--- src/gr/aueb/cf/ch2/IntApp.java
+++ src/gr/aueb/cf/ch2/IntApp.java
@@ -9,7 +9,7 @@ public class IntApp {
     public static void main(String[] args) {
         //To BYTES είναι το scope που ανήκει στο wrapper class Integer. Αυτό το υποδηλώνει η τελεία.
         //To , μετά το %d βάζει υποδιαστολή και χωρίζει ανά χιλιάδες
-        System.out.printf("int. Size: %d bits, Min: %,d, Max: %,d\n", Integer.BYTES * 8, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        System.out.printf("Το μέγεθος ενός Integer σε bytes είναι: %d, Το μέγεθος ενός Integer σε bits είναι: %d bits, Min: %,d, Max: %,d\n", Integer.BYTES, Integer.BYTES * 8, Integer.MIN_VALUE, Integer.MAX_VALUE);
         System.out.printf("byte. Size: %d bits, Min: %d, Max: %d\n", Byte.BYTES * 8, Byte.MIN_VALUE, Byte.MAX_VALUE);
         System.out.printf("short. Size: %d bits, Min: %,d, Max: %,d\n", Short.BYTES * 8, Short.MIN_VALUE, Short.MAX_VALUE);
         System.out.printf("long. Size: %d bits, Min: %,d, Max: %,d\n", Long.BYTES * 8, Long.MIN_VALUE, Long.MAX_VALUE);
