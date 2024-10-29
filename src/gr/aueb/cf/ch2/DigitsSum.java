dfb4be81a7fd591427d423082ea214cfa46f380e
fykoulas <spyrosfykas@gmail.com>
2024-10-29 20:39:28 +0200

    29/10/2024

--- src/gr/aueb/cf/ch2/DigitsSum.java
+++ src/gr/aueb/cf/ch2/DigitsSum.java
@@ -7,18 +7,20 @@ import java.util.Scanner;
  */
 public class DigitsSum {
     public static void main(String[] args) {
+        // Μεταβλητές και αρχικοποίηση
         Scanner in = new Scanner(System.in);
         int number = 0;
         int digitLeft = 0;
         int digitRight = 0;
         int sum = 0;
 
+        //Εισαγωγή από τον χρήστη
         System.out.println("Δώστε τον διψήφιο αριθμό: ");
         number = in.nextInt();
         digitLeft = number / 10;
         digitRight = number % 10;
         sum = digitLeft + digitRight;
-
+        //Εκτύπωση
         System.out.printf("Το άθροισμα είναι: %d", sum);
     }
 }
