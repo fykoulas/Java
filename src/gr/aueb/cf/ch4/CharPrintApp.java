314a1799b0f262e1153a061a585bfb98500136f7
fykoulas <spyrosfykas@gmail.com>
2024-11-05 21:02:24 +0200

    5/11/2024 - chapter 4

---
+++ src/gr/aueb/cf/ch4/CharPrintApp.java
@@ -0,0 +1,15 @@
+package gr.aueb.cf.ch4;
+
+/**
+ * Print chars
+ */
+public class CharPrintApp {
+    public static void main(String[] args) {
+        char ch1 = 'A';
+        char ch2 = '8';
+
+        System.out.println("char:" + ch1 + " Ordinal: " + (int) ch1);
+        System.out.println("char: " + ch2 + " Ordinal: " + (int) ch2);
+        System.out.printf("char: %c, Ordinal: %d\n", ch2, (int) ch2);
+    }
+}
