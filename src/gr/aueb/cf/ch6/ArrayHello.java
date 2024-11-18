a5d2be42f332ee3fcd5bf8487455bc50f986bff8
fykoulas <spyrosfykas@gmail.com>
2024-11-18 18:06:25 +0200

    12/11/2024

--- src/gr/aueb/cf/ch6/ArrayHello.java
+++ src/gr/aueb/cf/ch6/ArrayHello.java
@@ -2,16 +2,23 @@ package gr.aueb.cf.ch6;
 
 public class ArrayHello {
     public static void main(String[] args) {
-        int[] arr = new int[3]; // declare and init
+        int[] arr = new int[3];
+        int[] arr2 = new int[2];                        // declare and init
 
         //Populate
         arr[0] = 5;
         arr[1] = 10;
         arr[2] = 4;
+        arr2[0] = 12;
+        arr2[1] = 14;
 
         //Traverse
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i] + " ");
+            System.out.print(arr[i] + " ");
+        }
+
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
         }
     }
 }
