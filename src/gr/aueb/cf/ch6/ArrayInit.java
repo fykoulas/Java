b5f53a4ca3021a66bf49f4a2928461e936d6a203
fykoulas <spyrosfykas@gmail.com>
2024-11-18 19:54:16 +0200

    18/11/2024 arrays

---
+++ src/gr/aueb/cf/ch6/ArrayInit.java
@@ -0,0 +1,32 @@
+package gr.aueb.cf.ch6;
+
+/**
+ * Δήλωση και αρχικοποίηση πίνακα
+ * - new
+ * - unsized init
+ * - array initializer
+ * - print τα στοιχεία ενός πίνακα
+ */
+public class ArrayInit {
+    public static void main(String[] args) {
+        int[] arr = new int[3];                 // declare and init
+        int[] arr2 = {1,2,3,4,5};               //unsized init
+        int[] arr3 = new int[] {1,2,3,4,5};     //array initializer
+
+        //Populate
+        arr[0] = 5;
+        arr[1] = 10;
+        arr[2] = 4;
+
+
+        //Traverse
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        for (int el: arr) {
+            System.out.print(el + " ");
+        }
+
+    }
+}
---
+++ src/gr/aueb/cf/ch6/ArraysInMethods.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.ch6;
+
+/**
+ * Arrays and methods
+ */
+public class ArraysInMethods {
+    public static void main(String[] args) {
+
+    }
+
+    public static void printArray(int[] arr) {
+
+    }
+}
