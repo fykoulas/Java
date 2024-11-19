17685109b25ebd7f86be2904f36c58fbf87d251a
fykoulas <spyrosfykas@gmail.com>
2024-11-19 18:40:38 +0200

    19/11/2024 Sorting Arrays

---
+++ src/gr/aueb/cf/ch6/SelectionSort.java
@@ -0,0 +1,32 @@
+package gr.aueb.cf.ch6;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] selectionSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            // Find min
+            int minPosition = i;
+            int minValue = arr[minPosition];
+
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j] < minValue) {
+                    minValue = arr[j];
+                    minPosition = j;
+                }
+            }
+            // Swap
+            swap(arr, minPosition, i);
+        }
+        return arr;
+    }
+    public static void swap (int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
+
