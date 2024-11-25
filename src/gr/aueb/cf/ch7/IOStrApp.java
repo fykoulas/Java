004b3feac3b057101583d5f8d285d559ee03adde
fykoulas <spyrosfykas@gmail.com>
2024-11-25 19:12:28 +0200

    25/11/2024 playing with strings

---
+++ src/gr/aueb/cf/ch7/IOStrApp.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.ch7;
+
+import java.util.Scanner;
+
+public class IOStrApp {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s1 = "";
+        String s2 = "";
+
+        System.out.println("Please insert 2 strings: ");
+        s1 = in.next();     //διαβάζει μέχρι να βρεί white space (space, tabs \t, new lines \n)
+        s2 = in.nextLine(); //διαβάζει τα πάντα μέχρι να βρεί enter ή new line \n
+
+        System.out.printf("s1: %s\n", s1);
+        System.out.printf("s2: %s\n", s2);
+    }
+}
