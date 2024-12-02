21adbd17819f8a06d67002a7d869d8b3ba4cde13
fykoulas <spyrosfykas@gmail.com>
2024-12-02 20:42:47 +0200

    2/12/2024 File handling

---
+++ src/gr/aueb/cf/ch9/PrintStreamWithMethod.java
@@ -0,0 +1,30 @@
+package gr.aueb.cf.ch9;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+public class PrintStreamWithMethod {
+
+    public static void main(String[] args) {
+        try {
+            PrintStream ps = new PrintStream("C:/Users/spyro/Desktop/file8.txt", StandardCharsets.UTF_8);
+
+            printMessage(ps, "Hello Coding Factory!");
+            printMessage(System.out, "Hello CF!!");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Generic print method.
+     *
+     * @param ps        the PrintStream.
+     * @param message   the message to print.
+     */
+    public static void printMessage(PrintStream ps, String message) {
+        ps.println(message);
+    }
+}
