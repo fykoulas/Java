323d7c20be1bc4ee7ce62445b9f2a22dc8641f72
fykoulas <spyrosfykas@gmail.com>
2024-12-03 18:11:41 +0200

    2/12/2024 File handling

--- src/gr/aueb/cf/ch9/PrintStreamWithMethod.java
+++ src/gr/aueb/cf/ch9/PrintStreamWithMethod.java
@@ -1,5 +1,7 @@
 package gr.aueb.cf.ch9;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
@@ -7,11 +9,12 @@ import java.nio.charset.StandardCharsets;
 public class PrintStreamWithMethod {
 
     public static void main(String[] args) {
-        try {
-            PrintStream ps = new PrintStream("C:/Users/spyro/Desktop/file8.txt", StandardCharsets.UTF_8);
 
+        //try (PrintStream ps = new PrintStream("C:/tmp/file7-ps.txt", StandardCharsets.UTF_8)) {
+        try (PrintStream ps = new PrintStream(new FileOutputStream("C:/tmp/file7-ps.txt", true),
+                true, StandardCharsets.UTF_8)) {
             printMessage(ps, "Hello Coding Factory!");
-            printMessage(System.out, "Hello CF!!");
+            printMessage(System.out, "Hello CF!");
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -19,10 +22,10 @@ public class PrintStreamWithMethod {
     }
 
     /**
-     * Generic print method.
+     * Generic PrintStream method.
      *
-     * @param ps        the PrintStream.
-     * @param message   the message to print.
+     * @param ps        the input PrintStream
+     * @param message   the input message.
      */
     public static void printMessage(PrintStream ps, String message) {
         ps.println(message);
