76f383c52d05ac45770ace88c2510f0cf449662e
fykoulas <spyrosfykas@gmail.com>
2024-12-25 20:04:24 +0200

    25/12/2024 playing with java

---
+++ src/gr/aueb/cf/FiveProjects/FromFileToArray.java
@@ -0,0 +1,45 @@
+package gr.aueb.cf.FiveProjects;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+public class FromFileToArray {
+    public static void main(String[] args) {
+        String inFilePath = "C:/Users/spyro/Desktop/allnumbers.txt";
+
+        String line;
+        String[] tokens;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inFilePath))) {
+
+            while ((line = reader.readLine()) != null) {
+                tokens = line.split(",+\\s*");
+
+            for (String token : tokens)  {
+                 
+            }
+
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/gr/aueb/cf/FiveProjects/Project01.java
@@ -0,0 +1,7 @@
+package gr.aueb.cf.FiveProjects;
+
+public class Project01 {
+    public static void main(String[] args) {
+
+    }
+}
